bfbe7b86cae76b4529933d9ea2fe23890d30a65a
Rafael Barzotto <rafabarzotto@hotmail.com>
2016-02-13 13:10:32 -0200

    Up Mean + Start Angular Front-End

---
+++ brewery/controller.js
@@ -0,0 +1,79 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('brewery')
+		.controller('BreweryController', BreweryController);
+
+	BreweryController.$inject = ['BreweryService'];
+
+	function BreweryController(BreweryService) {
+		var vm = this;
+		vm.empty = {};
+
+		vm.findAll = function() {
+			BreweryService.findAll().then(
+				function(response) {
+					console.log(response);
+					vm.breweries = response.data;
+				},
+				function(error) {
+					console.log(error);
+				});
+		};
+
+		vm.findAll();
+
+		vm.reset = function() {
+			vm.brewery = angular.copy(vm.empty);
+		};
+
+		vm.save = function(brewery) {
+			if (brewery._id) {
+				BreweryService.update(brewery).then(
+					function(response) {
+						vm.success = response.data;
+						vm.findAll();
+						vm.reset();
+					},
+					function(error) {
+						console.log(error);
+						vm.error = error.data;
+					});
+			} else {
+				BreweryService.create(brewery).then(
+					function(response) {
+						vm.success = response.data;
+						vm.findAll();
+						vm.reset();
+					},
+					function(error) {
+						console.error(error);
+						vm.error = error.data;
+					}
+				);
+			}
+		};
+
+		vm.populate = function(brewery) {
+			vm.brewery = angular.copy(brewery);
+		};
+
+		vm.remove = function(brewery) {
+			if (confirm('Tem Certeza que gostaria de remover a cervejaria ' + brewery.name + ' ?')) {
+				BreweryService.remove(brewery._id).then(
+					function(response) {
+						vm.success = response.data;
+						vm.findAll();
+					},
+					function(error) {
+						console.log(error);
+						vm.error = error.data;
+					});
+			}
+		};
+
+
+
+	}
+})();
